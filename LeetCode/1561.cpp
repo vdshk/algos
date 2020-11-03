86e8a3b0ab184f8db43efc48e1353d5aa2654df4
viabzalov <vadim.i.abzalov@gmail.com>
2020-11-03 21:37:19 +0300

    Added 1561. Maximum Number of Coins You Can Get

---
+++ LeetCode/1561.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define len(a) (int)a.size()
+
+using namespace std;
+
+typedef vector<int> vint;
+typedef long double ld;
+typedef long long ll;
+typedef string str;
+
+class Solution
+{
+public:
+    int maxCoins(vector<int> &piles)
+    {
+        sort(all(piles));
+        int n = len(piles);
+        int i = 0;
+        int j = n - 2;
+        int k = n - 1;
+        int res = 0;
+        while (n > 0)
+        {
+            res += piles[j];
+            ++i;
+            j -= 2;
+            k -= 2;
+            n -= 3;
+        }
+        return res;
+    }
+};
