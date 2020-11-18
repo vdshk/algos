995ac4855fc8543c43968091b241e1d82c5797b8
viabzalov <vadim.i.abzalov@gmail.com>
2020-11-18 11:18:36 +0300

    Added Matches

---
+++ E-Olymp/8.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define len(a) (int)a.size()
+
+using namespace std;
+
+typedef vector<int> vint;
+typedef long double ld;
+typedef long long ll;
+typedef string str;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int s = sqrt(n);
+    cout << 2 * n + s + ((n + s - 1) / s);
+}
